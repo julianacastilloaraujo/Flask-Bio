3c64bc659698b4d17443fda9b93bdffecd434571
jlianacastillo <jcastilloa@ucundinamarca.edu.co>
2024-04-05 14:20:52 -0500

    cambios en persona.py

--- src/persona.py
+++ src/persona.py
@@ -1,12 +1,12 @@
 class Persona:
-    def __init__(self, nombre, apellido, telefono):
+    def __init__(self, nombre, correo, mensaje):
         self.nombre = nombre
-        self.apellido = apellido
-        self.telefono = telefono
+        self.correo = correo
+        self.mensaje = mensaje
 
     def formato_doc(self):
         return {
             'nombre': self.nombre,
-            'apellido': self.apellido,
-            'telefono': self.telefono
+            'correo': self.correo,
+            'mensaje': self.mensaje
         }
